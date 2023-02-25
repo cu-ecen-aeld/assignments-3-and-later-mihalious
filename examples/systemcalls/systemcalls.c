b1e55f6f0e7dd8d48f8467f604931e1da7e08ac4
misha <miha2003l78@gmail.com>
2023-02-25 23:42:04 +0200

    assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,8 @@
 #include "systemcalls.h"
+#include<stdlib.h>
+#include<unistd.h>
+#include<wait.h>
+#include<fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -9,14 +13,15 @@
 */
 bool do_system(const char *cmd)
 {
-
 /*
  * TODO  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
+    if(system(cmd) == -1) {
+        return false;
+    }
     return true;
 }
 
@@ -47,8 +52,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
-
+    //command[count] = command[count];
 /*
  * TODO:
  *   Execute a system command by calling fork, execv(),
@@ -58,10 +62,23 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
-
+    pid_t pid = fork();
+    if (pid == -1) {
+        return false;
+    }
+    if (pid == 0) {
+        execv(command[0], command);
+        exit(-1);
+    }
+    int wstatus;
+    if (waitpid(pid, &wstatus, 0) == -1) {
+        return false;
+    }
+    if (WIFEXITED(wstatus)) {
+        return WEXITSTATUS(wstatus) == 0;
+    }
     va_end(args);
-
-    return true;
+    return false;
 }
 
 /**
@@ -82,9 +99,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
-
-
+    //command[count] = command[count];
 /*
  * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
@@ -92,8 +107,34 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
-
+    int kidpid;
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) { 
+        perror("open"); 
+        return false; 
+    }
+    
+    switch (kidpid = fork()) {
+    case -1: 
+        perror("fork");
+        return false;
+    case 0:
+        if (dup2(fd, 1) < 0) {
+            perror("dup2");
+            return false;
+        }
+        close(fd);
+        execv(command[0], command);
+    default:
+        close(fd);
+        int wstatus;
+        if (waitpid(kidpid, &wstatus, 0) == -1) {
+            return false;
+        }
+        if (WIFEXITED(wstatus)) {
+            return WEXITSTATUS(wstatus) == 0;
+        }
+    }
     va_end(args);
-
     return true;
 }
