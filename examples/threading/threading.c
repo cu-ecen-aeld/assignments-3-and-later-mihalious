90b46f694a67eb679f7c81bdf7e3cec3e89a08b8
misha <miha2003l78@gmail.com>
2023-03-02 04:05:09 +0200

    assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,15 +10,26 @@
 
 void* threadfunc(void* thread_param)
 {
-
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    // struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* p_data = (struct thread_data *) thread_param;
+    usleep(p_data->wait_to_obtain_ms * 1000);
+    if (pthread_mutex_lock(p_data->mutex) != 0) {
+        p_data->thread_complete_success = false;
+        return thread_param;
+    }
+    usleep(p_data->wait_to_release_ms * 1000);
+    if (pthread_mutex_unlock(p_data->mutex) != 0) {
+        p_data->thread_complete_success = false;
+        return thread_param;
+    }
+    p_data->thread_complete_success = true;
     return thread_param;
 }
 
 
-bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
+bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
@@ -28,6 +39,22 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* p_data = (struct thread_data*)malloc(sizeof(struct thread_data));
+    if (p_data == NULL) {
+        //free(p_data);
+        return false;
+    }
+    p_data->thread = thread;
+    p_data->mutex = mutex;
+    p_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    p_data->wait_to_release_ms =wait_to_release_ms;
+    p_data->thread_complete_success = false;
+
+    if (pthread_create(thread, NULL, threadfunc, p_data) != 0) {
+        //free(p_data);
+        return false;
+    }    
+    //free(p_data);
+    return true;
 }
 
